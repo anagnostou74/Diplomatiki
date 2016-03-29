bb5a083a2de0b6ead4785f42e6cb6c928cd4ec84
Costas Anagnostou <anagnostou74@gmail.com>
2016-03-30 02:38:39 +0300

    CardView for rss, mps & bio

--- app/src/main/java/gr/mobap/mps/SingleItemView.java
+++ app/src/main/java/gr/mobap/mps/SingleItemView.java
@@ -48,18 +48,15 @@ public class SingleItemView extends MainActivity {
         super.onCreate(savedInstanceState);
         // Get the view from singleitemview.xml
         setContentView(R.layout.singleitemview);
-
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
 
         // [START shared_tracker]
         // Obtain the shared Tracker instance.
