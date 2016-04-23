d190703246f43dfe6e3cbb44a2a7ac6d76349bdd
Costas Anagnostou <anagnostou74@gmail.com>
2016-04-23 12:26:03 +0300

    Analytics

--- app/src/main/java/gr/mobap/web/SindesmoiActivity.java
+++ app/src/main/java/gr/mobap/web/SindesmoiActivity.java
@@ -13,13 +13,17 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 
+import com.google.android.gms.analytics.Tracker;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import gr.mobap.AnalyticsApplication;
 import gr.mobap.MainActivity;
 import gr.mobap.R;
 
 public class SindesmoiActivity extends MainActivity {
+    private Tracker mTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +44,13 @@ public class SindesmoiActivity extends MainActivity {
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        // [START shared_tracker]
+        // Obtain the shared Tracker instance.
+        AnalyticsApplication application = (AnalyticsApplication) getApplication();
+        mTracker = application.getDefaultTracker();
+        // [END shared_tracker]
+
     }
 
 
