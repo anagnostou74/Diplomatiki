81505d6dc6fa5c676cb845cb8ed2434da0c29114
Costas Anagnostou <anagnostou74@gmail.com>
2016-05-24 00:27:56 +0300

    Fb & rss photos for small screens

--- app/src/main/java/gr/mobap/Splash.java
+++ app/src/main/java/gr/mobap/Splash.java
@@ -4,11 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
 public class Splash extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
