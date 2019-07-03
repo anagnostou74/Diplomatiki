e44d5eccc44180100754a7e32103a5a1430683b2
Costas Anagnostou <anagnostou74@gmail.com>
2019-07-03 03:15:58 +0300

    UI: Proedros & SyntagmaFragment UI improvements

--- app/src/main/java/gr/mobap/komma/ProedrosFragment.java
+++ app/src/main/java/gr/mobap/komma/ProedrosFragment.java
@@ -1,14 +1,100 @@
 package gr.mobap.komma;
 
+import android.os.Build;
+import android.os.Bundle;
+import android.text.Html;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 
+import gr.mobap.R;
+import gr.mobap.mps.MpsAdapter;
+import gr.mobap.mps.MpsData;
 import gr.mobap.mps.MpsListFragment;
+import gr.mobap.mps.MpsViewHolder;
 
 public class ProedrosFragment extends MpsListFragment {
     public String TAG = getClass().getSimpleName();
+    private DatabaseReference mDatabase;
+    private RecyclerView mRecycler;
+    private LinearLayoutManager mManager;
+    private FirebaseRecyclerAdapter<MpsData, MpsViewHolder> mAdapter;
+    private TextView president;
 
     public ProedrosFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View rootView = inflater.inflate(R.layout.fragment_list_pr, container, false);
+
+        mRecycler = rootView.findViewById(R.id.mpsList);
+        mRecycler.setHasFixedSize(true);
+        // Initialize Database
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        president = rootView.findViewById(R.id.president);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            president.setText(Html.fromHtml(getString(R.string.proedreioKeim), Html.FROM_HTML_MODE_COMPACT));
+        } else {
+            president.setText(Html.fromHtml(getString(R.string.proedreioKeim)));
+        }
+
+        return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // [START create_database_reference]
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        // [END create_database_reference]
+
+        // Set up Layout Manager
+        mManager = new LinearLayoutManager(getActivity());
+        mRecycler.setLayoutManager(mManager);
+
+        // Set up FirebaseRecyclerAdapter with the Query
+        Query mpsQuery = getQuery(mDatabase);
+        Log.d(TAG, "Value is: " + mpsQuery);
+
+        FirebaseRecyclerOptions<MpsData> options = new FirebaseRecyclerOptions.Builder<MpsData>()
+                .setQuery(mpsQuery, MpsData.class)
+                .build();
+
+        mAdapter = new MpsAdapter(options, getContext());
+        mRecycler.setAdapter(mAdapter);
+        mAdapter.startListening();
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (mAdapter != null) {
+            mAdapter.startListening();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAdapter != null) {
+            mAdapter.stopListening();
+        }
     }
 
     @Override
--- app/src/main/java/gr/mobap/vouli/SyntagmaFragment.java
+++ app/src/main/java/gr/mobap/vouli/SyntagmaFragment.java
@@ -1,14 +1,15 @@
 package gr.mobap.vouli;
 
 import android.os.Bundle;
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 
 import gr.mobap.R;
 
@@ -29,10 +30,11 @@ public class SyntagmaFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_keimena, container, false);
 
-        final String htmlText = getResources().getString(R.string.syntagmaKeim);
+        final String htmlA = getResources().getString(R.string.syntagmaKeimA);
+        final String htmlB = getResources().getString(R.string.syntagmaKeimB);
         TextView tv = view.findViewById(R.id.textThesmos);
         tv.setMovementMethod(LinkMovementMethod.getInstance());
-        tv.setText(Html.fromHtml(htmlText));
+        tv.setText(Html.fromHtml(htmlA + htmlB));
         // Inflate the layout for this fragment
         return view;
     }
